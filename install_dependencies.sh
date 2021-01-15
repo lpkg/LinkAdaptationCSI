9c595129f15ac2c2f103fbf23097b5d12df3efdd
Sleeba Paul <sleebapaul@gmail.com>
2021-01-15 12:43:53 +0530

    Update shell file

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,8 +1,11 @@
-#!/bin/zsh
+#!/bin/bash
 
 pip3 install -r requirements.txt
 git clone https://github.com/vidits-kth/py-itpp.git 
-./py-itpp/install_prerequisites_python3.sh
+cd py-itpp 
+ls .
+./install_prerequisites_python3.sh
 make install  
-pip3 install -e .  
+pip3 install -e .
+cd ..
 rm -rf py-itpp 
